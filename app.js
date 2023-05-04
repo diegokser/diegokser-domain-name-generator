9e978db8c230b572bd2342b8d81d2f7174875acb
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-04 02:36:35 +0000

    cambios en archivo app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-let pronoun = ['the','our', 'your'];
+let pronoun = ['the', 'our', 'your'];
 let adj = ['great', 'big', 'length'];
-let noun = ['jogges','racoom','rancio'];
-let extension = ['.com','.es', '.io'];
+let noun = ['jogges', 'racoom', 'rancio', 'potatoes'];
+let domain = ['.com', '.es', '.io'];
 
 pronoun.forEach(element => {
     adj.forEach(element1 => {
         noun.forEach(element2 => {
             domain.forEach(element3 => {
-                console.log(element + element1 + element2 + element3);
+                let newNoun = element2;
+                if (element2.endsWith("es")) {
+                    newNoun = element2.replace("es", "");
+                }
+                console.log(element + element1 + newNoun + element3);
             });
         });
     });
